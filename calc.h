4542289aeed4ac66f1611d390eb97592be0cdd52
wij <41879253+wijiler@users.noreply.github.com>
2023-05-29 15:44:57 -0400

    Add files via upload

---
+++ calc.h
@@ -0,0 +1,5 @@
+#define CALC
+#ifndef CALC
+float weaponweightcalc(unsigned int weaponkills, unsigned int weapondeaths, float weaponkd);
+float overallweightcalc(unsigned int kills, unsigned int deaths, float kd);]
+#endif
---
+++ weight.c
@@ -0,0 +1,6 @@
+#include "calc.h"
+int main()
+{
+
+    return 0;
+}
